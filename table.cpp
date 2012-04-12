d4f62169faafd663affeb4a73ef4870203324787
mbrittan@nd.edu <mbrittan@nd.edu@b1a83a2c-0e0c-7e43-24db-203b03b0cb9a>
2012-04-12 01:47:31 +0000

    Changed to only use one card. Trying to get the move() fcn in.

--- table.cpp
+++ table.cpp
@@ -1,19 +1,56 @@
 #include "table.h"
 #include <QImage>
 #include <QString>
-#include <Q3Painter>
-using namespace std;
+#include <QPainter>
+#include <QWidget>
+#include <QGridLayout>
+//#include <QTGui>
+#include <QLabel>
 
-
-Table::Table(QWidget *parent): QWidget (parent)
+//Constructor for table that opens a window
+Table::Table(QWidget *parent, const char *name) : QMainWindow(parent)
+  //ui(new Ui::Table)
 {
-    tablePic= new QImage;
-    tablePic->load("holdEmTable.jpg");
+  setWindowTitle(tr("Welcome to Hold 'Em!")); //titles window
+  resize(991, 441); //resizes window to fit background
+  fileName=name;
 }
-
 
 void Table::paintEvent(QPaintEvent *)
 {
-    QPainter painter(this);  //! get a painter object to send drawing commands to
-    painter.drawImage(0,0, *tablePic);
+  //Portion that creates background image
+  //Uses a pixmap and two QrectFs to draw an image on the background
+  //Painter things for table
+  QRectF targetTable(0, 0, 991, 441);
+  QRectF sourceTable(0, 0, 991, 441);
+  QPixmap pixMapTable("holdEmTable.png");
+
+  QPainter painter(this);
+  painter.drawPixmap(targetTable, pixMapTable, sourceTable);
+
+  //Draws Card
+  QRect targetCard(255, 160, 72, 97);
+  QPixmap pixMapCard(fileName);
+  painter.drawPixmap(targetCard, pixMapCard);  
+  
+  moveCard(targetCard, pixMapCard);
 }
+
+void Table:: moveCard(QRect card, QPixmap pixMap)
+{
+  QRect targetCard=card;
+  QPixmap pixMapCard=pixMap;
+  QPainter painter(this);
+
+  for (int i=0; i<=100; i++)
+    {
+      targetCard.translate(1, 0);
+      
+    }
+}
+
+Table::~Table()
+{
+ 
+}
+
