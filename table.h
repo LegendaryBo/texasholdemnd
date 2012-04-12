8e6bdc009c93f1df4a67d24408bd007dd7bd34d0
mbrittan@nd.edu <mbrittan@nd.edu@b1a83a2c-0e0c-7e43-24db-203b03b0cb9a>
2012-04-12 01:48:16 +0000

    Changed table.h to incorporate one card and moving the card.

--- table.h
+++ table.h
@@ -1,20 +1,38 @@
 #ifndef TABLE_H
 #define TABLE_H
 
-#include "lab8_global.h"
+#include <QMainWindow>
 #include <QWidget>
 #include <QImage>
 #include <QString>
 #include <QPainter>
+#include <QGridLayout>
+#include <QLabel>
+#include <string>
+#include <QWidget>
 using namespace std;
 
-class Table : public QWidget {
+
+//Relatively unsure what this does (came from QT creator)
+namespace Ui {
+  class Table;
+}
+
+//Creates "Table" class, which is background and card display on table
+class Table : public QMainWindow
+{
     Q_OBJECT
-public:
-    Table(QWidget *parent = 0);
-    void paintEvent(QPaintEvent *);
-private:
-     QImage *tablePic;
+
+      public:
+  Table(QWidget *parent = 0, const char *name=""); //Constructor
+  void paintEvent(QPaintEvent*); //Event that does the painting
+  ~Table(); //Deconstructor
+  void moveCard(QRect, QPixmap);
+    
+ private:
+  const char *fileName;
+  int velocity;
 };
 
 #endif // TABLE_H
+
