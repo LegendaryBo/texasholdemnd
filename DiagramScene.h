e6a6449a9afc120c40abc5ac7e089e0d34782b0d
mbrittan@nd.edu <mbrittan@nd.edu@b1a83a2c-0e0c-7e43-24db-203b03b0cb9a>
2012-04-23 12:46:30 +0000

    This is the display itself. Really helpful

---
+++ DiagramScene.h
@@ -0,0 +1,63 @@
+#ifndef DIAGRAMSCENE_H
+#define DIAGRAMSCENE_H
+#include <QGraphicsScene>
+#include <QObject>
+
+class QGraphicsScene;
+class QGraphicsSceneMouseEvent;
+class QMenu;
+class QPointF;
+class QGraphicsLineItem;
+class QFont;
+class QGraphicsTextItem;
+class QColor;
+class QPixMap;
+class QGraphicsPixmapItem;
+
+class DiagramScene : public QGraphicsScene
+{
+  Q_OBJECT
+    
+    public:
+  
+  DiagramScene(QObject *parent=0);
+  QGraphicsScene* getScene();
+  void addItem();
+  void clearHand();
+  void drawBackground();
+  void setCaseInt(int);
+  void dealInitialHand(const char *, const char *);
+  void dealPlayer(const char *, const char *);
+  void dealComp();
+  void dealFlop(const char *, const char *, const char *);
+  void dealTurn(const char*);
+  void dealRiver(const char *);
+  void nextState();
+  void showComp(const char *, const char *);
+    
+  
+  //public slots:
+  
+  //signals:
+  
+  //protected:
+    
+  private:
+  
+  int caseInt;
+  QGraphicsScene *graphicsScene;
+  
+  QPixmap *pixMap;
+  
+  QGraphicsPixmapItem *item1Ptr;
+  QGraphicsPixmapItem *item2Ptr; 
+  QGraphicsPixmapItem *item3Ptr; 
+  QGraphicsPixmapItem *item4Ptr;
+  QGraphicsPixmapItem *item5Ptr;
+  QGraphicsPixmapItem *item6Ptr;
+  QGraphicsPixmapItem *item7Ptr;
+  QGraphicsPixmapItem *item8Ptr;
+  QGraphicsPixmapItem *item9Ptr;
+};
+
+#endif
