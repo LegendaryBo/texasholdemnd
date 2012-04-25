d6a7391bb03b56f6f43ac1c054827fea63915369
kjacobs3@nd.edu <kjacobs3@nd.edu@b1a83a2c-0e0c-7e43-24db-203b03b0cb9a>
2012-04-25 05:21:54 +0000

    Window.h

---
+++ windowKev.h
@@ -0,0 +1,74 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+#include <QMainWindow>
+#include <QGraphicsScene>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QFormLayout>
+#include <QAction>
+#include "DiagramScene.h"
+
+//class DiagramScene;
+
+class QAction;
+class QListWidget;
+class QMenu;
+class QLineEdit;
+class QPushButton;
+class QFormLayout;
+class QGraphicsView;
+class QWidget;
+class QPixmap;
+class QGraphicsScene;
+//class DiagramScene;
+
+class Window : public QMainWindow
+{
+  Q_OBJECT
+    
+    public:
+  Window();
+  
+  private slots:
+  
+  void bet();
+  void call();
+  void raise();
+  void fold();
+  void check();
+  
+  private:
+  
+  
+  //Background
+
+  DiagramScene *scene;
+  QGraphicsScene *graphicsScene;
+  QGraphicsView *graphicsView;
+  
+  void createActions();
+  void createButtons();                         
+  void createDockWindows();
+  void createLayout();
+  
+  QPushButton *betButton;
+  QPushButton *raiseButton;
+  QPushButton *callButton;
+  QPushButton *foldButton;
+  QPushButton *checkButton;
+  
+  QLineEdit *lineEditBet;
+  QLineEdit *lineEditRaise;
+  QLineEdit *lineEditCall;
+
+  QFormLayout *layout;
+
+  QAction *betAction;
+  QAction *callAction;
+  QAction *raiseAction;
+  QAction *foldAction;
+  QAction *checkAction;
+};
+
+#endif
+
