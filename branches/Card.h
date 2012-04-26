5784aa6689cfbae37839be6b4559df6e2a22c496
saufderh@nd.edu <saufderh@nd.edu@b1a83a2c-0e0c-7e43-24db-203b03b0cb9a>
2012-04-26 19:44:45 +0000

    card with the right variable for the filenames

---
+++ branches/Card.h
@@ -0,0 +1,36 @@
+// texas holdem, cards
+
+#ifndef CARD_H
+#define CARD_H
+
+#include <iostream>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+class Card{
+
+public:
+
+Card(int = 0, const char* = "card");
+
+const char *cardname;    //image file                   
+
+int value;   // kings, aces, sixes, etc
+
+int number;  //number of card in deck
+
+int suit;   //diamond, clubs etc
+
+};
+
+
+Card::Card(int n, char i){
+
+number = n;
+value = n%13;
+suit = n/13;
+cardname = i;
+}
+
+#endif
