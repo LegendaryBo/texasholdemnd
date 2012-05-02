ba7fc7d021eb721b1518e9cf0015a2dfe4f59c8d
mbrittan@nd.edu <mbrittan@nd.edu@b1a83a2c-0e0c-7e43-24db-203b03b0cb9a>
2012-05-02 05:40:40 +0000

    Few comments added, capitalizations added

--- game.h
+++ game.h
@@ -6,32 +6,38 @@
 #include "Card.h"
 #include "player.h"
 
+extern deque < Card > community;
+extern int potsize;
+extern int highbet;
+extern int bigblind;
+extern int Round;
 
 
-class game
+class Game
 {
 
 public:
-  game ();			//input is number of players
-   ~game ();
-  CardDeck Deck;		// need to add dealtop() member function to CardDeck
-  deque < player* > position;	// can be ignored for now, but position of players rotates
-  user human;
-  computer AI;
-  void winner ();		// if more than 1 player remains, uses rank function for each
-  void newhand ();		// shuffles deck, clears potsize, clears community cards
+  Game ();
+  ~Game ();
+  CardDeck Deck;        
+  deque < Player* > position;//position of players rotates
+  User human;
+  Computer AI;
+  void winner (); // if more than 1 player remains, uses rank function for each
+  void newhand ();//shuffles deck, clears potsize, clears community cards
   void rungame ();
-  int play;			// when 1 game keeps going, when 0 games ends
+  int play;// when 1 game keeps going, when 0 games ends
   void dealallcards();
   void dealpreflop ();
   void dealflop ();
   void dealturn ();
   void dealriver ();
-  int preflop ();		// returns 1 if more than 1 player remains
-  int flop ();			// returns 1 if more than 1 player remains
-  int turn ();			// returns 1 if more than 1 player remains
-  int river ();			// returns 1 if more than 1 player remains
-  void reset ();		// clear community/player cards, potsize=0; rotates position
+  int preflop ();// returns 1 if more than 1 player remains
+  int flop (); // returns 1 if more than 1 player remains
+  int turn ();// returns 1 if more than 1 player remains
+  int river ();    // returns 1 if more than 1 player remains
+  void reset (); // clear community/player cards, potsize=0; rotates position
 };
 
 #endif
+
