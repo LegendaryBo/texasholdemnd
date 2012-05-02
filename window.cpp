fe9b3ebe3eb6521b736e24c8e8ebe095fcf6ad2f
mbrittan@nd.edu <mbrittan@nd.edu@b1a83a2c-0e0c-7e43-24db-203b03b0cb9a>
2012-05-02 05:37:00 +0000

    Some lineEdits changed to labels, comments added

--- window.cpp
+++ window.cpp
@@ -1,3 +1,14 @@
+//window.cpp
+//Authors: Kevin Jacobs, Matt Brittan, Scott Aufderheide
+/*Description: This program opens a QT window to display the dock widgets 
+(where all the buttons are held) and also holds the DiagramScene where the 
+actual game scene is displayed. The dock widget is controlled/manipulated
+through this scene, while the DiagramScene is simply displayed, and mainly
+manipulated through its own class; however, a few calls are made to 
+DiagramScene functions within the buttons functions. On that note, whenever
+buttons are clicked, they call an action that updates graphics and calls
+functions within DiagramScene.cpp. This will be shown below (in comments)*/ 
+
 #include <QtGui>
 #include <QAction>
 #include <QGraphicsScene>
@@ -10,64 +21,129 @@
 
 Window::Window()
 {
-  setWindowTitle(tr("Dock Widgets"));
+  setWindowTitle(tr("Texas Hold 'Em"));
   resize(1258, 500);
 
+  //Declares new DiagramScene to display game
+  //Actually displays it through a QGraphicsView which holds a QGraphicsScene
   scene = new DiagramScene(this);
   graphicsScene = scene->getScene();
-
   graphicsView = new QGraphicsView(graphicsScene);
   setCentralWidget(graphicsView);
   
-  createButtons();
+  //Creates all widgets
+  createButtonsandEdits();
   createLayout();
   createDockWindows();
   createActions();
+
+  //Updates the count of chips to start the game 
+  updateCompChips();
+  updateHumanChips();
+  updatePot();
 }
 
-
-void Window::createButtons()
+//Creates buttons and the text boxes to display chip interaction
+void Window::createButtonsandEdits()
 {
+  //Blank QLineEdit used for formatting
+  lineEditBlank = new QLineEdit();
+  lineEditBlank -> setVisible(false);
+
+  //Used to set font for labels to display headers
+  QFont f( "Arial", 11, QFont::Bold);
+
+  //Header for player section
+  lineEditPlayer = new QLabel();
+  lineEditPlayer -> setText("Player");
+  lineEditPlayer -> setAlignment(Qt::AlignCenter);
+  lineEditPlayer -> setFont(f);
+
+  //Header for computer section
+  lineEditComputer = new QLabel();
+  lineEditComputer -> setText("Computer");
+  lineEditComputer -> setAlignment(Qt::AlignCenter);
+  lineEditComputer -> setFont(f);
+  
+  //Betting button and line edit
   betButton = new QPushButton("Bet");
   lineEditBet = new QLineEdit;
   lineEditBet->setText("50");
+
+  //Raise button and line edit
   raiseButton = new QPushButton("Raise");
   lineEditRaise = new QLineEdit;
-  lineEditBet->setText("50");
+  
   callButton = new QPushButton("Call");
-  lineEditCall = new QLineEdit;
+ 
   foldButton = new QPushButton("Fold");
+
   checkButton = new QPushButton("Check");
-
-  updateButton = new QPushButton("Make Computer Action");
-  lineEditUpdate = new QLineEdit;
-
-  infoButton = new QPushButton("Info");
-  lineEditInfo = new QLineEdit;
+  
+  //Human chips labels
+  lineEditHumanChips = new QLabel;
+  lineEditHumanChips->setText(" Chips Remaining: ");
+  lineEditHumanChipCount = new QLabel;
+     
+  compActionButton = new QPushButton("Make Computer Action!");
+  lineEditCompShow = new QLineEdit;
+  
+  //Comp chips labels
+  lineEditCompChips = new QLabel;
+  lineEditCompChips -> setText(" Chips Remaining: ");
+  lineEditCompChipCount = new QLabel;
+  
+  //Pot chips labels
+  lineEditPotChips = new QLabel;
+  lineEditPotChips -> setText(" Pot Chip Count: ");
+  lineEditShowPot = new QLabel;
 }
 
+//Creates a layout to store the buttons neatly in
+//Adds multiple buttons/line edits to a row using addRow
 void Window::createLayout()
 {
   layout = new QFormLayout;
+  
+  layout->addRow(lineEditPlayer);
+  layout->addRow(lineEditBlank);
   layout->addRow(betButton, lineEditBet);
   layout->addRow(raiseButton, lineEditRaise);
-  layout->addRow(callButton, lineEditCall);
-  layout->addRow(foldButton);
-  layout->addRow(checkButton);
-  layout->addRow(updateButton, lineEditUpdate);
-  layout->addRow(infoButton, lineEditInfo);
+  layout->addRow(callButton, lineEditBlank);
+  layout->addRow(checkButton, lineEditBlank);
+  layout->addRow(foldButton, lineEditBlank);
+  layout->addRow(lineEditBlank);
+  layout->addRow(lineEditHumanChips, lineEditHumanChipCount);
+  layout->addRow(lineEditBlank);
+  layout->addRow(lineEditBlank);
+  layout->addRow(lineEditBlank);
+  layout->addRow(lineEditBlank);
+  layout->addRow(lineEditComputer);
+  layout->addRow(lineEditBlank);
+  layout->addRow(compActionButton);
+  layout->addRow(lineEditCompShow);
+  layout->addRow(lineEditBlank);
+  layout->addRow(lineEditCompChips, lineEditCompChipCount);
+  layout->addRow(lineEditBlank);
+  layout->addRow(lineEditBlank);
+  layout->addRow(lineEditBlank);
+  layout->addRow(lineEditBlank);
+  layout->addRow(lineEditPotChips, lineEditShowPot);
 }
 
+//Creates a dock window to create interactivity with buttons
 void Window::createDockWindows()
 {
   QWidget *wi = new QWidget;
   wi->setLayout(layout);
   
-  QDockWidget *dw = new QDockWidget(tr("Buttons"), this);
+  QDockWidget *dw = new QDockWidget(this);
   dw->setWidget(wi);
   addDockWidget(Qt::LeftDockWidgetArea, dw);
 }
 
+//Creates actions
+//Every time a button is "clicked", connects to a function that runs
 void Window::createActions()
 {
   connect(betButton, SIGNAL(clicked()), this, SLOT(bet()));
@@ -75,95 +151,138 @@ void Window::createActions()
   connect(callButton, SIGNAL(clicked()), this, SLOT(call()));
   connect(foldButton, SIGNAL(clicked()), this, SLOT(fold()));
   connect(checkButton, SIGNAL(clicked()), this, SLOT(check()));
-  connect(updateButton, SIGNAL(clicked()), this, SLOT(update()));
-  connect(infoButton, SIGNAL(clicked()), this, SLOT(info()));
+  connect(compActionButton, SIGNAL(clicked()), this, SLOT(updateCompAction()));
 }
 
-
-void Window::update()
+//Shows the user what the computer's move was whenever the "Make Computer Move"
+//button is clicked
+void Window::updateCompAction()
 {
-  char str[9];
-  int x;
-  x=scene->updateCall();
-  //x=scene->AIChoice();
-  sprintf(str,"%d", x);
-
-  lineEditUpdate->setText(str);
- // if(scene->getCaseInt() == 0) scene->nextState ();
+  char str[60];
+  int AIbet = scene->Holdem.AI.betamount;
+  char choice = scene->Holdem.AI.choice;
+  
+  //Decides what to display in computer move text box
+  if (choice=='b') sprintf(str, "Computer bets %d", AIbet);
+  else if (choice =='f') sprintf(str, "Computer folds!");
+  else if (choice == 'p') sprintf(str, "Computer checks!");
+  else if (choice == 'r') sprintf(str, "Computer raises %d", AIbet);
+  else if (choice == 'c') sprintf(str, "Computer calls");
+  else if (choice == 'w') sprintf(str, "Welcome to the game! Click again!");
+  
+  updatePot();
+  updateCompChips();
+  updateHumanChips();
+  
+  lineEditCompShow->setText(str);
+  scene->update();
+  graphicsScene->update();
+  graphicsView->update();
+  
+  //Moves to next state if necessary
+  //Function in DiagramScene
   if(scene->getCaseInt() == -1) scene->nextState ();
   else if(scene->getCaseInt() == 8) scene->nextState();
+  
 }
 
-void Window::info()
+//Updates the text string for pot chips
+void Window::updatePot()
 {
-   char str[40];
-   int AIbet = scene->Holdem.human.betamount;
-   int AIchips = scene->Holdem.human.chips;
-   int pot = potsize;
-   double humanrank = scene->Holdem.human.rank;
-   double AIrank = scene->Holdem.AI.rank;
-   int humancard = scene->Holdem.human.findhigh(3);
-   int AIcard = scene->Holdem.AI.findhigh(3);
-   int caseint = scene->getCaseInt();
-   char choice = scene->Holdem.AI.choice;
-   sprintf(str, "AI=%f,human=%f,choice=%c",AIrank,humanrank,choice);
+  char str[50];
+  int pot = potsize;
+  sprintf(str, "  %d", pot);
 
-  lineEditInfo->setText(str);
-  scene->update();
-  graphicsScene->update();
-  graphicsView->update();
+  lineEditShowPot -> setText(str);
+}
+ 
+//Updates text string for comp chips
+void Window::updateCompChips()
+{
+  char str[50];
+  int chips = scene->Holdem.AI.chips;
+  sprintf(str, "  %d", chips);
 
+  lineEditCompChipCount -> setText(str);
 }
 
+//Updates text box for human chips
+void Window::updateHumanChips()
+{
+  char str[50];
+  int chips = scene->Holdem.human.chips;
+  sprintf(str, "  %d", chips);
 
+  lineEditHumanChipCount->setText(str);
+}
+
+//Function called when bet button's clicked
+//calls bet() function in DiagramScene, updates graphics
 void Window::bet()
 { 
-if(scene->Holdem.AI.choice != 'r' && scene->Holdem.AI.choice != 'b') {
-  scene->bet(lineEditBet->text().toInt());
-  scene->update();
-  graphicsScene->update();
-  graphicsView->update();
-}
+  //if functions as safeguard
+  if(scene->Holdem.AI.choice != 'r' && scene->Holdem.AI.choice != 'b') 
+    {
+      scene->bet(lineEditBet->text().toInt());
+      updateHumanChips();
+      scene->update();
+      graphicsScene->update();
+      graphicsView->update();
+    }
 }
 
+//Called when raise button clicked
+//Calls DiagramScene raise() function, updates chip textboxes
 void Window::raise()
 {
-int raiseamount;    
-if(scene->Holdem.AI.choice == 'r' || scene->Holdem.AI.choice == 'b') {
-raiseamount = lineEditRaise->text().toInt();
-if(raiseamount > highbet) {
-  scene->raise(raiseamount);
-  scene->update();
-  graphicsScene->update();
-  graphicsView->update();
-}// end check if raise is valid
-}// end check if available option
+  int raiseamount;    
+  if(scene->Holdem.AI.choice == 'r' || scene->Holdem.AI.choice == 'b') 
+    {
+      raiseamount = lineEditRaise->text().toInt();
+      if(raiseamount > highbet) 
+    {
+      scene->raise(raiseamount);
+      updateHumanChips();
+      scene->update();
+      graphicsScene->update();
+      graphicsView->update();
+    }// end check if raise is valid
+    }// end check if available option
 }// end function
 
+//Called when call button clicked
+//Calls DiagramScene call function, updates chips and updates scene
 void Window::call()
 {
 if(scene->Holdem.AI.choice == 'r' || scene->Holdem.AI.choice == 'b') {
   scene->call();
+  updateHumanChips();
   scene->update();
   graphicsScene->update();
   graphicsView->update();
 }
 }
 
+//Same as others
 void Window::fold()
 {
   if(scene->Holdem.AI.choice != 'p')
     {
-  scene->Holdem.human.choice='f';
-  graphicsScene->update();
-  graphicsView->update();
-}
+      scene->Holdem.human.choice='f';
+      updateHumanChips();
+      graphicsScene->update();
+      graphicsView->update();
+    }
 }
 
+//Same as others
 void Window::check()
 {
-if (scene->Holdem.AI.choice != 'r' && scene->Holdem.AI.choice != 'b') {
-  scene->check();
+  if (scene->Holdem.AI.choice != 'r' && scene->Holdem.AI.choice != 'b') 
+    {
+      scene->check();
+      updateHumanChips();
+  }
 }
-}
+
 
