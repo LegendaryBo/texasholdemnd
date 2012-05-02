7758d9b5d0c60da994c3aeb499cf7d662c80fa15
mbrittan@nd.edu <mbrittan@nd.edu@b1a83a2c-0e0c-7e43-24db-203b03b0cb9a>
2012-05-02 05:35:19 +0000

    Comments added

--- DiagramScene.h
+++ DiagramScene.h
@@ -1,9 +1,15 @@
+//DiagramScene.h
+//Authors: Matt Brittan, Kevin Jacobs, Scott Aufderheide
+//Description in .cpp implementation
+
 #ifndef DIAGRAMSCENE_H
 #define DIAGRAMSCENE_H
+
 #include <QGraphicsScene>
 #include <QObject>
 #include "game.h"
 
+//Classes included for definitions
 class QGraphicsScene;
 class QGraphicsSceneMouseEvent;
 class QMenu;
@@ -15,7 +21,7 @@ class QColor;
 class QPixMap;
 class QGraphicsPixmapItem;
 
-
+//Extends QGraphicsScene for functionality
 class DiagramScene : public QGraphicsScene
 {
   Q_OBJECT
@@ -24,20 +30,24 @@ class DiagramScene : public QGraphicsScene
   
   DiagramScene(QObject *parent=0);
   QGraphicsScene* getScene();
-  void addItem();
-  void clearHand();
-  void drawBackground();
-  void setCaseInt(int);
-  int getCaseInt();
-  void dealInitialHand(const char *, const char *);
-  void dealPlayer(const char *, const char *);
-  void dealComp();
-  void dealFlop(const char *, const char *, const char *);
+  void clearHand(); //Clears all pics on board by deleting their pointers
+  void drawBackground(); //Draws table
+  void setCaseInt(int); //Sets int for case function in nextState
+  int getCaseInt(); //Returns int for case function in nextState
+  
+  //All cards dealt through fileNames
+  void dealInitialHand(const char *, const char *); //Deals two initial cards
+  void dealPlayer(const char *, const char *); //Deals two cards to player
+  void dealComp(); //Shows two backgrounds
+  void dealFlop(const char *, const char *, const char *); //Deals three cards
   void dealTurn(const char*);
   void dealRiver(const char *);
-  void nextState();
   void showComp(const char *, const char *);
+  
+  //Where major case decision making is done
+  void nextState();
 
+  //Functions that display image for amount of chips
   void displayChips();
   void displayPlayerChips();
   void displayCompChips();
@@ -49,22 +59,19 @@ class DiagramScene : public QGraphicsScene
   void call();
   void check();
   
-  int updateCall();
-  int AIChoice();
+  int updateCall(); //Returns amount of bet to help player call amount
+  int AIChoice(); //Returns choice AI made
+   
+  Game Holdem; //Instance of Game to play hold 'em
   
-  //public slots:
-  
-  //signals:
-  
-  //protected:
-  game Holdem; // move back to private    
-  private:
+ private:
 
   int caseInt;
   QGraphicsScene *graphicsScene;
   
   QPixmap *pixMap;
   
+  //Pointers to images created
   QGraphicsPixmapItem *playerCardPtr1;
   QGraphicsPixmapItem *playerCardPtr2; 
   QGraphicsPixmapItem *cardBackgroundPtr1; 
@@ -80,7 +87,7 @@ class DiagramScene : public QGraphicsScene
   QGraphicsPixmapItem *compChipsPtr;
   QGraphicsPixmapItem *potPtr;
   
-
+  //Cutoffs for chips image
   int smallChips;
   int smallMediumChips;
   int mediumLargeChips;
@@ -88,3 +95,6 @@ class DiagramScene : public QGraphicsScene
 };
 
 #endif
+
+
+
