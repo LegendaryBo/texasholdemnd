112960ec203f3437938b8baf189e0c2acaa4208b
kjacobs3@nd.edu <kjacobs3@nd.edu@b1a83a2c-0e0c-7e43-24db-203b03b0cb9a>
2012-05-02 01:32:24 +0000

    qwe

--- player.cpp
+++ player.cpp
@@ -3,12 +3,12 @@
 
 player::player ()
 {
-  chips = 1000;
+  chips = 100000;
 }
 
-player::~player()
+player::~player ()
 {
-delete this;
+  delete this;
 }
 
 
@@ -16,11 +16,26 @@ delete this;
 double
 player::Rank ()
 {
+  rank = 0;
   //them from smallest to greatest in value // these are ordered in the findhigh function
   for (int i = 0; i < 5; i++)
     combined.push_back (community[i]);
   for (int i = 0; i < 2; i++)
     combined.push_back (holecards[i]);
+// sort:
+  int j;
+  Card temp (0);
+  for (int i = 0; i < 7; i++)
+    {
+      j = i;
+      while (j > 0 && combined[j - 1].value > combined[j].value)
+    {
+      temp = combined[j];
+      combined[j] = combined[j - 1];
+      combined[j - 1] = temp;
+      j--;
+    }
+    }
   Card Pair = pair ();
   if (Pair.value != -1)
     {
@@ -32,7 +47,8 @@ player::Rank ()
   straight ();
   flush ();
   if (straight () && flush ())
-    rank = 8;
+  rank = 8;
+  combined.clear ();
   return rank;
 }
 
@@ -41,7 +57,8 @@ int
 player::bet (int bettotal)
 {
   if (bettotal > chips)
-    betamount = chips;		// cant bet more than you have
+    betamount = chips;        // cant bet more than you have
+  betamount = bettotal;
   chips -= betamount;
   potsize += betamount;
   return chips;
@@ -50,47 +67,66 @@ player::bet (int bettotal)
 int
 player::findhigh (int high)
 {
-// sort:
-  int j;
-  Card temp(0);
-  for (int i = 0; i < 7; i++)
-    {
-      j = i;
-      while (j > 0 && combined[j - 1].value > combined[j].value)
-	{
-	  temp = combined[j];
-	  combined[j] = combined[j - 1];
-	  combined[j - 1] = temp;
-	  j--;
-	}
-    }
   return combined[7 - high].value;
 }
 
 
 Card player::pair ()
 {
-  Card high(0);
+  Card
+  high (0);
   high.value = -1;
+  double
+    thirdCard = 0;
+  double
+    fourthCard = 0;
+  double
+    fifthCard = 0;
   double
     tempRank = 0;
   for (int i = 0; i < 6; i++)
     {
       if (combined[i].value == combined[i + 1].value)
-	high = combined[i];
+    high = combined[i];
     }
   if (high.value != -1)
-    tempRank = 1 + double (high.value) /
-  100 + double (findhigh (3)) /
-  10000 + double (findhigh (4)) /
-  1000000 + double (findhigh (5)) /
+    {
+      if (findhigh (1) == high.value)
+    {
+      thirdCard = findhigh (3);
+      fourthCard = findhigh (4);
+      fifthCard = findhigh (5);
+    }
+      else if (findhigh (2) == high.value)
+    {
+      thirdCard = findhigh (1);
+      fourthCard = findhigh (4);
+      fifthCard = findhigh (5);
+    }
+      else if (findhigh (3) == high.value)
+    {
+      thirdCard = findhigh (1);
+      fourthCard = findhigh (2);
+      fifthCard = findhigh (5);
+    }
+      else if (findhigh (4) == high.value)
+    {
+      thirdCard = findhigh (1);
+      fourthCard = findhigh (2);
+      fifthCard = findhigh (3);
+    }
+      tempRank = 1 + double (high.value) /
+      100 + double (thirdCard) /
+      10000 + double (fourthCard) /
+      1000000 + double (fifthCard) /
     100000000;
+    }
   else
-  tempRank = double (findhigh (1)) /
+    tempRank = double (findhigh (1)) /
   100 + double (findhigh (2)) /
-  10000 + double (findhigh (3)) /
-  1000000 + double (findhigh (4)) /
-  100000000 + double (findhigh (5)) /
+  10000 + double (findhigh(3)) /
+  1000000 + double (findhigh(4)) /
+  100000000 + double (findhigh(5)) /
     10000000000;
 
   if (tempRank > rank)
@@ -102,26 +138,32 @@ int
 player::twopair (Card pair)
 {
   double tempRank = 0;
+  double fifthcard = 0;
   Card high;
   high.value = -1;
   for (int i = 0; i < 6; i++)
     {
       if ((combined[i].value == combined[i + 1].value)
-	  && (combined[i].value != pair.value))
-	high = combined[i];
+      && (combined[i].value != pair.value))
+    high = combined[i];
     }
   if (high.value == -1)
-    return 0;			// two pair not found
+    return 0;            // two pair not found
   else
     {
+      for (int i = 1; i < 6; i++)
+    {
+      if (findhigh (i) != high.value && findhigh (i) != pair.value)
+        fifthcard = findhigh (i);
+    }
       if (high.value > pair.value)
-	tempRank =
-	  2 + double (high.value) / 100 + double (pair.value) / 10000 +
-	double (findhigh (5)) / 1000000;
+    tempRank =
+      2 + double (high.value) / 100 + double (pair.value) / 10000 +
+    double (fifthcard) / 1000000;
       else
       tempRank =
-	2 + double (pair.value) / 100 + double (high.value) / 10000 +
-	double (findhigh (5)) / 1000000;
+    2 + double (pair.value) / 100 + double (high.value) / 10000 +
+    double (fifthcard) / 1000000;
     }
   if (tempRank > rank)
     rank = tempRank;
@@ -134,109 +176,138 @@ player::trips ()
   double tempRank = 0;
   Card high;
   high.value = -1;
+  double fourthcard = -1;
+  double fifthcard = -1;
   for (int i = 1; i < 6; i++)
     {
       if (combined[i - 1].value == combined[i].value
-	  && combined[i].value == combined[i + 1].value)
-	{
-	  high = combined[i];
-	}
+      && combined[i].value == combined[i + 1].value)
+    {
+      high = combined[i];
     }
-      if (high.value == -1)
-	return 0;
-      else
-	tempRank = 3 + double (high.value) / 100 + double (findhigh (4)) / 10000 + double (findhigh (5)) / 1000000;	// othercards() wont work, digits off
+    }
+  if (high.value == -1)
+    return 0;
+  else
+    {
+      for (int i = 1; i < 6; i++)
+    {
+      if (fourthcard == -1 && findhigh (i) != high.value)
+        fourthcard = findhigh (i);
+      else if (findhigh (i) != fourthcard && fourthcard != -1 && findhigh (i) != high.value)
+        fifthcard = findhigh (i);
+    }
+      tempRank = 3 + double (high.value) / 100 + (fourthcard) / 10000 + (fifthcard) / 1000000;    // othercards() wont work, digits off
       if (tempRank > rank)
-	rank = tempRank;
+    rank = tempRank;
       return 1;
+    }
 }
 
-
-  int player::quads ()
-  {
-    double tempRank = 0;
-    Card high;
-    high.value = -1;
-    for (int i = 0; i < 4; i++)
-      {
-	if (combined[i].value == combined[i + 1].value &&  combined[i + 1].value==
-	    combined[i + 2].value && combined[i + 2].value == combined[i + 3].value)
-	  high = combined[i];
-      }
-    if (high.value == 0)
-      return 0;
-    else {
+int
+player::quads ()
+{
+  double tempRank = 0;
+  Card high;
+  double fifthcard = -1;
+  high.value = -1;
+  for (int i = 0; i < 4; i++)
+    {
+      if (combined[i].value == combined[i + 1].value
+      && combined[i + 1].value == combined[i + 2].value
+      && combined[i + 2].value == combined[i + 3].value)
+    high = combined[i];
+    }
+  if (high.value == -1)
+    return 0;
+  else
+    {
+if(findhigh(1) == high.value) fifthcard = findhigh(5);
+else fifthcard = findhigh(1);
       tempRank =
-	7 + double (high.value) / 100 + double (findhigh (5)) / 10000; }
-    if (tempRank > rank)
-      rank = tempRank;
-    return 1;
-  }
+    7 + double (high.value) / 100 + double (fifthcard) / 10000;
+    }
+  if (tempRank > rank)
+    rank = tempRank;
+  return 1;
+}
 
-  int player::straight ()
-  {
-    double tempRank = 0;
-    int total = 1;
-    Card high;
-    high.value = -1;
-    for (int i = 0; i < 3; i++)
-      {
-	if (combined[i].value == combined[i + 1].value - 1)
-	  {
-	    total++;
-	    high = combined[i + 1];
-	  }
-	else if (total < 5 && combined[i].value != combined[i + 1].value)
-	  total = 1;
-      }				// does nothing of adjacent cards are equal
-    if (total > 4)
-      tempRank = 4 + double (high.value / 100);
-    else
+int
+player::straight ()
+{
+  double tempRank = 0;
+  int total = 1;
+  Card high;
+  high.value = -1;
+  bool AcePresent = 0;
+for(int i = 0; i < 7; i++) {
+if(combined[i].value == 12) AcePresent = 1;
+}
+
+  for (int i = 0; i < 6; i++)
+    {
+      if (combined[i].value == combined[i + 1].value - 1)
+    {
+      total++;
+      high = combined[i + 1];
+    }
+      else if (combined[i].value == 3 && AcePresent && (combined[i-1].value == 2 || combined[i-1].value == 3)) {
+      total++;
+      high.value = 12;
+      AcePresent = 0;
+    }
+      else if (total < 5 && combined[i].value != combined[i + 1].value)
+    total = 1;
+      else if (total > 4 && combined[i].value != combined[i+1].value) break;
+    }    
+  if (total > 4)
+    tempRank = 4 + double (high.value) / 100;
+  else
+  return 0;
+
+  if (tempRank > rank)
+    rank = tempRank;
+  return 1;
+
+}
+
+int
+player::flush ()
+{
+  double tempRank = 0;
+  int total;
+  Card high;
+  for (int i = 0; i < 4; i++)
+    {
+      total = 0;
+      for (int j = 0; j < 7; j++)
+    {
+      if (combined[j].suit == i)
+        {
+          total++;
+          high = combined[j];
+        }
+    }
+      if (total > 4)
+    {tempRank = 5 + double (high.value) / 100; break;}
+    }
+  if (tempRank < 5)
     return 0;
 
-    if (tempRank > rank)
       rank = tempRank;
-    return 1;
 
-  }
+  return 1;
+}
 
-  int player::flush ()
-  {
-    double tempRank = 0;
-    int total;
-    Card high;
-    for (int i = 0; i < 4; i++)
-      {
-	total = 0;
-	for (int i = 0; i < 7; i++)
-	  {
-	    if (combined[i].suit == i)
-	      {
-		total++;
-		high = combined[i];
-	      }
-	  }
-	if (total > 4)
-	  tempRank = 5 + double (high.value / 100);
-      }
-    if (tempRank < 5)
-      return 0;
-    if (tempRank > rank)
-      {
-	rank = tempRank;
-      }
-    return 1;
-  }
-
-  int player::fullhouse (Card Pair)
-  {
-    double tempRank = 0;
-    if (twopair (Pair) && trips ())
-      tempRank = 6.1;
-    else return 0;
-    if (tempRank > rank)
-      rank = tempRank;
-    return 1;
-  }
-
-
+int
+player::fullhouse (Card Pair)
+{
+  double tempRank = 0;
+  if (twopair (Pair) && trips ())
+    tempRank = 6.1;
+  else
+    return 0;
+  if (tempRank > rank)
+    rank = tempRank;
+  return 1;
+}
